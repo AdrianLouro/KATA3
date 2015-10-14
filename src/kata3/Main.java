db280096aef48437a413095015e0d891be4c056e
usuario <usuario@10.22.143.9>
2015-10-14 09:52:12 +0100

    Primera versión (datos sin fichero).

---
+++ src/kata3/Main.java
@@ -0,0 +1,31 @@
+package kata3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Histogram<String> histograma = new Histogram<>();
+        histograma.increment("gmail.com");
+        histograma.increment("gmail.com");
+        histograma.increment("gmail.com");
+        histograma.increment("gmail.com");
+        
+        histograma.increment("ulpgc.es");
+        histograma.increment("ulpgc.es");
+        histograma.increment("ulpgc.es");
+        histograma.increment("ulpgc.es");
+        histograma.increment("ulpgc.es");
+        histograma.increment("ulpgc.es");
+        histograma.increment("ulpgc.es");
+        
+        histograma.increment("hotmail.com");
+        histograma.increment("hotmail.com");
+        histograma.increment("hotmail.com");
+        histograma.increment("hotmail.com");
+        histograma.increment("hotmail.com");
+        histograma.increment("hotmail.com");
+        histograma.increment("hotmail.com");
+        
+        HistogramDisplay histo = new HistogramDisplay(histograma);
+        histo.execute();
+    }
+}
